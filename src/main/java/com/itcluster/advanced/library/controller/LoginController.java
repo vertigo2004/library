b583e4757051ba0452edd00ba12c92a5f5bae1c0
Oleh Pashkevych <vertigo.if@gmail.com>
2019-09-17 22:24:32 +0300

    Regustration form validation is extended.

--- src/main/java/com/itcluster/advanced/library/controller/LoginController.java
+++ src/main/java/com/itcluster/advanced/library/controller/LoginController.java
@@ -9,36 +9,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
-public class LoginController {
+public class LoginController implements WebMvcConfigurer {
 
-    private static final String EMPTY_FIELD_MESSAGE = "This field can't be empty";
     private static final String EMAIL_ALREADY_USED_MESSAGE ="There is already a user registered with the email provided";
     private static final String SUCCESSFULLY_REGISTERED_MESSAGE = "User has been registered successfully";
 
     @Autowired
     private UserService userService;
 
-    @GetMapping(value={"/login"})
-    public String login() {
-        return "login";
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registration").setViewName("registration");
     }
 
-    @GetMapping("/registration")
-    public String registration(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "registration";
+    @ModelAttribute("user")
+    public User activeUser(Authentication authentication) {
+        return new User();
     }
 
     @PostMapping("/registration")
     public String createNewUser(@Valid User user, BindingResult bindingResult, Model model) {
-        // todo: add form validation on empty fields
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
         Optional<User> userExists = userService.findUserByEmail(user.getEmail());
         if (userExists.isPresent()) {
             bindingResult.rejectValue("email", "error.user", EMAIL_ALREADY_USED_MESSAGE);
--- src/main/java/com/itcluster/advanced/library/model/User.java
+++ src/main/java/com/itcluster/advanced/library/model/User.java
@@ -8,6 +8,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 import java.util.Set;
 
@@ -18,9 +20,14 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank(message = "Must not be blank")
+    @Email(message = "Must be a valid email address")
     private String email;
+    @NotBlank(message = "Must not be blank")
     private String password;
+    @NotBlank(message = "Must not be blank")
     private String firstname;
+    @NotBlank(message = "Must not be blank")
     private String lastname;
     private Date dob;
     private String photo;
